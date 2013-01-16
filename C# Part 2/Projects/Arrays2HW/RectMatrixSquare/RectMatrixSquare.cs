7ff6644ae2644c721ceecb7069ecac4595a8eb0e
Nikolay Genov <nikolay.genov2@gmail.com>
2013-01-16 22:11:02 +0200

    Update

---
+++ C# Part 2/Projects/Arrays2HW/RectMatrixSquare/RectMatrixSquare.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class RectMatrixSquare
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter N (rows):");
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter M (cols)");
+        int m = int.Parse(Console.ReadLine());
+        //check if m and N <  3
+        int[,] matrix = new int[n, m];
+        EnterNumbers(n, m, matrix);
+
+       
+    }
+  
+    private static void EnterNumbers(int n, int m, int[,] matrix)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                Console.Write("Matrix [{0}/{1},{2}/{3}]= ", i + 1, n, j + 1, m);
+                matrix[i, j] = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
+
