4248852641b464f80859ac5a48aaa9123ccac5bb
unknown <nikolay.genov2@gmail.com>
2013-01-28 19:27:51 +0200

    Git problem To solve

---
+++ Knowledge Sharing/TestGit/TestProblem/TestProblem/TestProblem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class TestProblem
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a number ");
+        //We multiply by 10
+        int mult = 10;
+        long sum = 1;
+        for (int i = 0; i < mult; i++)
+        {
+            sum *= mult;
+            
+        }
+        Console.WriteLine(sum);
+
+        //Create a loop to add +1 to the sum mult times
+
+
+    }
+}
