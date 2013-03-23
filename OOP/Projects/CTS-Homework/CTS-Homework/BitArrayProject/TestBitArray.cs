0da9a243f84eec413e3de147143a8ba51b25bc0a
Nikolay Genov <nikolay.genov2@gmail.com>
2013-03-23 18:12:54 +0200

    Almost Done CTS homework

---
+++ OOP/Projects/CTS-Homework/CTS-Homework/BitArrayProject/TestBitArray.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitArrayProject
+{
+    class TestBitArray
+    {
+        static void Main(string[] args)
+        {
+            BitArray64 testOne = new BitArray64(423423523);
+            BitArray64 testTwo = new BitArray64(1);
+            BitArray64 testThree = new BitArray64(9);
+
+            Console.WriteLine(testOne == testTwo);
+
+            Console.WriteLine(testTwo.Equals(testThree));
+        }
+    }
+}
