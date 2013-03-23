211f0ef35bd9051fae3a76d53f65e5521a48299e
Nikolay Genov <nikolay.genov2@gmail.com>
2013-03-23 16:32:03 +0200

    CTS HW more work done

---
+++ OOP/Projects/CTS-Homework/CTS-Homework/ProjectPerson/Person.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace ProjectPerson
+{
+    class Person
+    {
+        public string Name { get; set; }
+
+        public byte? Age { get; set; }
+        
+        public Person(string name, byte? age =null)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder info = new StringBuilder();
+            info.AppendFormat("Name: {0}", this.Name).AppendLine();
+            if (this.Age != null)
+            {
+                info.AppendFormat("Age: {0}", this.Age).AppendLine();
+            }
+            else
+            {
+                info.AppendLine("The age is not specified");
+            }
+            return info.ToString();
+        }
+    }
+}
---
+++ OOP/Projects/CTS-Homework/CTS-Homework/ProjectPerson/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ProjectPerson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person personOne = new Person("Gosho Peshov", 30);
+            Person personTwo = new Person("Pesho Goshov");
+            Console.WriteLine(personOne);
+            Console.WriteLine(personTwo);
+        }
+    }
+}
