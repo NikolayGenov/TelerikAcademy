24e641ad7d9830a99ac9c8f2ebe00909e48f3816
Nikolay Genov <nikolay.genov2@gmail.com>
2013-03-24 23:12:35 +0200

    Big part of the preparation finished

---
+++ OOP/Projects/ExamPrep/Video.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DocumentSystem
+{
+    public class Video : MultimediaDocs
+    {
+        public double? FrameRate { get; set; }
+
+        public override void LoadProperty(string key, string value)
+        {
+            if (key == "framerate")
+            {
+                this.FrameRate = double.Parse(value);
+            }
+            else
+            {
+                base.LoadProperty(key, value);
+            }
+        }
+
+        public override void SaveAllProperties(IList<KeyValuePair<string, object>> output)
+        {
+            base.SaveAllProperties(output);
+            output.Add(new KeyValuePair<string, object>("framerate", this.FrameRate));
+        }
+    }
+}
