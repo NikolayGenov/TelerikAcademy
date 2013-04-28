b698ca650257de03b36eccde5cdd0e4b862a3e8b
Nikolay Genov <nikolay.genov2@gmail.com>
2013-04-28 20:18:38 +0300

    KPK UsingVars HW

---
+++ KPK/Projects/5.UsingVariablesDataExpressionsConstants/UsingVariablesDataExpressionsConstants/Point.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+/// <summary>
+/// Stores a point of a given object with X and Y properties and can be rotated with a given angle.
+/// </summary>
+public class Point
+{
+    public Point(double x, double y)
+    {
+        this.X = x;
+        this.Y = y;
+    }
+
+    public double X { get; private set; }
+
+    public double Y { get; private set; }
+
+    public static Point Rotate(Point point, double rotationAngle)
+    {
+        double cosAngle = Math.Abs(Math.Cos(rotationAngle));
+        double sinAngle = Math.Abs(Math.Sin(rotationAngle));
+        double newX = (cosAngle * point.X) + (sinAngle * point.Y);
+        double newY = (sinAngle * point.X) + (cosAngle * point.Y);
+        Point rotatedPoint = new Point(newX, newY);
+        return rotatedPoint;        
+    }
+}
---
+++ KPK/Projects/5.UsingVariablesDataExpressionsConstants/UsingVariablesDataExpressionsConstants/Statistics.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+/// <summary>
+/// Class containing methods for finding the maximum, minimum and average from given array and given count.
+/// </summary>
+public class Statistics
+{
+    public void PrintStatistics(double[] array, int count)
+    {
+        PrintMax(Max(array, count));
+        PrintMin(Min(array, count));
+        PrintAvg(Average(array, count));
+    }
+
+    private static double Max(double[] array, int count)
+    {
+        if (array == null || array.Length == 0)
+        {
+            throw new ArgumentException("Invalid array"); 
+        }
+
+        if (count <= 0)
+        {
+            throw new ArgumentException("Invalid range number");
+        }
+
+        double maxValue = array[0];
+        for (int i = 0; i < count; i++)
+        {
+            if (array[i] > maxValue)
+            {
+                maxValue = array[i];
+            }
+        }
+
+        return maxValue;
+    }
+
+    private static double Min(double[] array, int count)
+    {
+        if (array == null || array.Length == 0)
+        {
+            throw new ArgumentException("Invalid array"); 
+        }
+
+        if (count <= 0)
+        {
+            throw new ArgumentException("Invalid range number");
+        }
+   
+        double minValue = array[0];
+        for (int i = 0; i < count; i++)
+        {
+            if (array[i] < minValue)
+            {
+                minValue = array[i];
+            }
+        }
+            
+        return minValue;
+    }
+
+    private static double Average(double[] array, int count)
+    {
+        if (array == null || array.Length == 0)
+        {
+            throw new ArgumentException("Invalid array"); 
+        }
+
+        if (count <= 0)
+        {
+            throw new ArgumentException("Invalid range number");
+        }
+
+        double sum = 0;
+        for (int i = 0; i < count; i++)
+        {
+            sum += array[i];
+        }
+
+        double average = sum / count;
+        return average;
+    }
+}
