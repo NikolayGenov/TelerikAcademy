52a1ac90f409c619fbc91e0e2f9e33e77e0dddd3
Nikolay Genov <nikolay.genov2@gmail.com>
2013-05-07 17:39:48 +0300

    KPK High Quality Classes HW

---
+++ KPK/Projects/8. High-Quality-Classes-Homework/Abstraction/Rectangle.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Abstraction
+{
+    class Rectangle : Figure
+    {
+        private double width = 0;
+        private double height = 0;
+
+        public Rectangle(double width, double height) 
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Width
+        {
+            get
+            {
+                return this.width;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("The width of the rectangle can't be less than 0");
+                }
+                else
+                {
+                    this.width = value;
+                }
+            }
+        }
+
+        public double Height
+        {
+            get
+            {
+                return this.height;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("The height of the rectangle can't be less than 0");
+                }
+                else
+                {
+                    this.height = value;
+                }
+            }
+        }
+
+        public override double CalcPerimeter()
+        {
+            double perimeter = 2 * (this.Width + this.Height);
+            return perimeter;
+        }
+
+        public override double CalcSurface()
+        {
+            double surface = this.Width * this.Height;
+            return surface;
+        }
+    }
+}
